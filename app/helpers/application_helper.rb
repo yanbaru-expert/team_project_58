a273840d19910084756dc1147edc58e924a00357
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2021-09-21 16:52:14 +0900

    新規登録、ログイン、アカウント編集ページ修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   def max_width
     if controller_name == "texts" && action_name == "show"
       "mw-md"
-    # Devise 導入後にコメントアウトを解除
     elsif devise_controller?
       "mw-sm"
     else
