1bfdc82e8c7d0d77f745f78a4e134a0f1142397c
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2021-09-22 15:07:43 +0900

    ゲストログインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root "texts#index"
   resources :texts, only: [:index, :show]
   resources :movies, only: [:index]
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
